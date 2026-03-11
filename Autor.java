93db0320d0ae21e5f34eb76101fe5405995eb9d9
Ale172400 <sanchezsantillanl@gmail.com>
2026-03-10 20:19:13 -0600

    Add Autor entity with fields and constructors

---
+++ Autor.java
@@ -0,0 +1,25 @@
+package com.literalura.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private Integer nacimiento;
+    private Integer muerte;
+
+    public Autor(){}
+
+    public Autor(String nombre, Integer nacimiento, Integer muerte) {
+        this.nombre = nombre;
+        this.nacimiento = nacimiento;
+        this.muerte = muerte;
+    }
+
+    // getters y setters
+}
